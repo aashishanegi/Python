0aeea9602e3ca4c21d2003b38d7ac64137b9487c
aashishanegi <91939200+aashishanegi@users.noreply.github.com>
2023-04-22 21:24:54 +0530

    Account Class

---
+++ account.py
@@ -0,0 +1,15 @@
+class account:
+    def __init__(self,initial_amount):
+        p=initial_amount
+    def value(self,p):
+        balance=p
+        return p
+    def withdraw(self,amount):
+        a=value()-amount
+    def deposit(self,amount):
+        b=value()+amount
+
+ac=account(1000)
+ac.balance=2000
+ac.balance=1000
+print(ac.balance)
